24c7dfb6374c7e8b7765e713db46cee4df9d5191
thai <privat_thai_nguyen@hotmail.com>
2026-01-26 21:44:28 +0200

    changed internal url to backend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import Index from "~/contents";
 import type { Route } from "./+types/home";
 import { createTodo, updateTodoItem, getAllTodos } from "./forms/todo";
 
-const serviceUrl = "http://127.0.0.1:3001";
+const serviceUrl = "http://todo-app-service:3001";
 
 export async function loader() {
   return await getAllTodos(serviceUrl);
