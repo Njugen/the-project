5cb87b89ab15ea71ab89d4462ec58220fc3ac94f
thai <privat_thai_nguyen@hotmail.com>
2026-01-30 12:49:40 +0200

    test

--- backend/scripts/utils/nats.js
+++ backend/scripts/utils/nats.js
@@ -11,43 +11,32 @@ nc.subscribe("MAPPER_STATUS", (message) => {
 });
 
 const checkTopic = async (subject) => {
-    console.log("A")
     const ready = await new Promise((resolve, reject) => {
-        console.log("B")
         const subscription = nc.subscribe(subject, (message) => {
-            console.log("C")
             if (!message) {
                 reject('No listeners found')
                 return
             }
-            console.log("D")
             resolve("Subscribed successfully")
-            console.log("E")
         })
-        console.log("F")
         resolve("Failed to check subscription");
-        console.log("G")
     })
 }
 
 const sendDataToNATS = async (subject, message, completed) => {
     const status = completed ? "COMPLETED" : "NOT COMPLETED"
-    console.log("a")
     try {
-        console.log("b")
         await checkTopic(subject);
-        console.log("c")
     } catch (err) {
-        console.log("d")
         console.error(err);
         return;
     }
-    console.log("e")
+
     const payload = {
         user: "system",
         message: `${message} (${status})`,
     }
-    console.log("f")
+
     nc.publish(subject, JSON.stringify(payload));
 }
 
--- broadcaster/index.js
+++ broadcaster/index.js
@@ -10,31 +10,40 @@ const nc = NATS.connect(
 let isBusy = false;
 const isProduction = process.env.DEPENV === 'production';
 const forwardToExternalService = async (message) => {
+    console.log("e")
     isBusy = true;
+    console.log("f")
     try {
-
+        console.log("g")
         if (isProduction) {
+            console.log("h")
             await fetch(process.env.MOCKAPIURL, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(message)
             });
+            console.log("i")
             nc.publish('MAPPER_STATUS', JSON.stringify({ user: 'broadcaster', message: 'Message forwarded' }))
         } else {
+            console.log("j")
             console.log(`\n`)
             console.log(`From ${message.user}:\n`)
             console.log(`${message.message}\n`)
             console.log(`\n`)
         }
     } catch (error) {
+        console.log("k")
         console.error("Error forwarding to external service:", error);
     }
     isBusy = false;
 }
 
 nc.subscribe('MAPPER_DATA', { queue: 'mapper.workers' }, async (message) => {
+    console.log("a")
     if (isBusy) return;
+    console.log("b")
     const data = JSON.parse(message);
+    console.log("c", data)
     await forwardToExternalService(data);
 });
 
